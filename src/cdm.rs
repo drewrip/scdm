57877534fce248201842e381407f762c931e36c2
Drew Ripberger <drew.ripberger@gmail.com>
2025-04-18 11:43:40 -0400

    parsing the index of the ndjson file

--- src/cdm.rs
+++ src/cdm.rs
@@ -1,14 +1,32 @@
-/// SQL table definitions that match the OpenSearch CDM indices
+use chrono::{DateTime, TimeDelta, Utc};
+use uuid::Uuid;
+
+#[derive(Clone, Debug)]
+pub enum Class {
+    Count,
+    Throughput,
+}
+
 pub const SQL_TABLE_ITERATION: &str = r#"
     CREATE TABLE IF NOT EXISTS iteration (
         iteration_uuid uuid PRIMARY KEY,
-        num bigint,
+        num bigint NOT NULL,
         status text,
         path text,
         primary_metric text,
         primary_period text
     )
 "#;
+
+#[derive(Clone, Debug)]
+pub struct Iteration {
+    pub iteration_uuid: Uuid,
+    pub num: i64,
+    pub status: Option<String>,
+    pub path: Option<String>,
+    pub primary_metric: Option<String>,
+    pub primary_period: Option<String>,
+}
 
 pub const SQL_TABLE_METRIC_DATA: &str = r#"
     CREATE TABLE IF NOT EXISTS metric_data (
@@ -22,6 +40,16 @@ pub const SQL_TABLE_METRIC_DATA: &str = r#"
     )
 "#;
 
+#[derive(Clone, Debug)]
+pub struct MetricData {
+    pub metric_data_id: u64,
+    pub metric_desc_uuid: Uuid,
+    pub begin: DateTime<Utc>,
+    pub finish: DateTime<Utc>,
+    pub duration: TimeDelta,
+    pub value: f64,
+}
+
 pub const SQL_TABLE_METRIC_DESC: &str = r#"
     CREATE TABLE IF NOT EXISTS metric_desc (
         metric_desc_uuid uuid PRIMARY KEY,
@@ -29,22 +57,36 @@ pub const SQL_TABLE_METRIC_DESC: &str = r#"
         type text,
         source text,
         names_list text,
-        names text,
-        value_format text,
-        values text
+        names text
     )
 "#;
 
-/// Much of this is WIP, not sure what these fields type should be
+#[derive(Clone, Debug)]
+pub struct MetricDesc {
+    pub metric_desc_uuid: Uuid,
+    pub class: Class,
+    pub metric_type: String,
+    pub source: String,
+    pub names_list: Vec<String>,
+    pub names: Vec<(String, String)>,
+}
+
 pub const SQL_TABLE_PARAM: &str = r#"
     CREATE TABLE IF NOT EXISTS param (
-        param_uuid uuid PRIMARY KEY,
-        id text,
+        iteration_uuid uuid,
         arg text,
-        role text,
-        val text
+        val text,
+        PRIMARY KEY(iteration_uuid, arg, val),
+        CONSTRAINT fk_iteration FOREIGN KEY (iteration_uuid) REFERENCES iteration(iteration_uuid)
     )
 "#;
+
+#[derive(Clone, Debug)]
+pub struct Param {
+    pub iteration_uuid: Uuid,
+    pub arg: String,
+    pub val: String,
+}
 
 pub const SQL_TABLE_PERIOD: &str = r#"
     CREATE TABLE IF NOT EXISTS period (
@@ -56,14 +98,20 @@ pub const SQL_TABLE_PERIOD: &str = r#"
     )
 "#;
 
+#[derive(Clone, Debug)]
+pub struct Period {
+    pub period_uuid: Uuid,
+    pub begin: DateTime<Utc>,
+    pub finish: DateTime<Utc>,
+    pub name: String,
+}
+
 pub const SQL_TABLE_RUN: &str = r#"
     CREATE TABLE IF NOT EXISTS run (
         run_uuid uuid PRIMARY KEY,
         begin timestamp,
         finish timestamp,
-        harness text,
         benchmark text,
-        host text,
         email text,
         name text,
         description text,
@@ -71,6 +119,18 @@ pub const SQL_TABLE_RUN: &str = r#"
         source text
     )
 "#;
+
+#[derive(Clone, Debug)]
+pub struct Run {
+    pub run_uuid: Uuid,
+    pub begin: DateTime<Utc>,
+    pub finish: DateTime<Utc>,
+    pub benchmark: String,
+    pub email: String,
+    pub name: String,
+    pub tags: Vec<(String, String)>,
+    pub source: String,
+}
 
 pub const SQL_TABLE_SAMPLE: &str = r#"
     CREATE TABLE IF NOT EXISTS sample (
@@ -81,6 +141,14 @@ pub const SQL_TABLE_SAMPLE: &str = r#"
     )
 "#;
 
+#[derive(Clone, Debug)]
+pub struct Sample {
+    pub sample_uuid: Uuid,
+    pub num: i64,
+    pub status: String,
+    pub path: Option<String>,
+}
+
 pub const SQL_TABLE_TAG: &str = r#"
     CREATE TABLE IF NOT EXISTS tag (
         tag_uuid uuid PRIMARY KEY,
@@ -88,3 +156,10 @@ pub const SQL_TABLE_TAG: &str = r#"
         val text
     )
 "#;
+
+#[derive(Clone, Debug)]
+pub struct Tag {
+    pub tag_uuid: Uuid,
+    pub name: String,
+    pub val: String,
+}
--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,95 @@
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
-use std::path::Path;
+use std::fs;
+use std::fs::File;
+use std::io::{BufReader, prelude::*};
+use std::path::{Path, PathBuf};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ParseError {
     #[error("Couldn't find path, or it isn't a directory: {0}")]
     InvalidPath(String),
+    #[error("Failed to deserialize {0}: {1}")]
+    JSONParseFailed(String, String),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct IndexJson {
+    pub index: IndexSpec,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct IndexSpec {
+    pub _index: String,
+}
+
+#[derive(Clone, Debug)]
+pub enum IndexType {
+    Iteration,
+    MetricData,
+    MetricDesc,
+    Param,
+    Period,
+    Run,
+    Sample,
+    Tag,
+}
+
+fn is_ndjson(path: &str) -> bool {
+    let length = path.len();
+    let extension = path.get(length - 7..length);
+    match extension {
+        Some(ext) => ext == ".ndjson",
+        None => false,
+    }
 }
 
 pub async fn parse(pool: &PgPool, dir_path: &Path) -> Result<()> {
-    let res = sqlx::query!("SELECT (1) as c").fetch_all(pool).await?;
+    // Read all of the ndjson files
+    let files = fs::read_dir(dir_path).map_err(|_| {
+        ParseError::InvalidPath(
+            dir_path
+                .to_str()
+                .map(|s| s.to_string())
+                .unwrap_or(format!("{:?}", dir_path)),
+        )
+    })?;
+
+    let paths = files
+        .into_iter()
+        .filter(|f| f.is_ok())
+        .filter_map(|f| f.ok())
+        .map(|d| d.path());
+
+    let ndjson_paths: Vec<PathBuf> = paths
+        .filter(|p| p.to_str().map(is_ndjson).unwrap_or(false))
+        .collect();
+
+    for ndjson_path in ndjson_paths {
+        let f = File::open(ndjson_path.clone()).map_err(|_| {
+            ParseError::InvalidPath(format!(
+                "Couldn't open file {}",
+                ndjson_path.to_str().unwrap_or("path")
+            ))
+        })?;
+
+        let reader = BufReader::new(f);
+        let mut lines = reader.lines();
+        while let (Some(Ok(index_jsonl)), Some(Ok(body_jsonl))) = (lines.next(), lines.next()) {
+            let index: IndexJson = serde_json::from_str(&index_jsonl)
+                .map_err(|e| ParseError::JSONParseFailed("IndexJSON".to_string(), e.to_string()))?;
+            println!("Index: {:?}", index);
+            println!("Body: {}", body_jsonl);
+        }
+    }
+
+    // Ingest the documents in one transaction
+    let mut txn = pool.begin().await?;
+    let res = sqlx::query!("SELECT (1) as c").fetch_all(&mut *txn).await?;
+
+    txn.commit().await?;
     println!("{:?}", res);
     Ok(())
 }
